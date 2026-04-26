5ceeefe097585d8192cd45a87ef9cdb97ed1f269
izum00 <karisute14@gmail.com>
2026-04-26 10:20:55 +0900

    Refactor VmdWriter.py for code consistency

--- applications/VmdWriter.py
+++ applications/VmdWriter.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import struct
+import math
 from PyQt6.QtGui import QQuaternion, QVector3D
 
 class VmdBoneFrame():
@@ -13,7 +14,7 @@ class VmdBoneFrame():
     def write(self, fout):
         name = self.name.encode('ms932')
         fout.write(name)
-        fout.write(bytearray([0 for i in range(len(name), 15)])) # ボーン名15Byteの残りを\0で埋める
+        fout.write(bytearray([0 for i in range(len(name), 15)]))
         fout.write(struct.pack('<L', self.frame))
         fout.write(struct.pack('<f', self.position.x()))
         fout.write(struct.pack('<f', self.position.y()))
@@ -23,7 +24,7 @@ class VmdBoneFrame():
         fout.write(struct.pack('<f', v.y()))
         fout.write(struct.pack('<f', v.z()))
         fout.write(struct.pack('<f', v.w()))
-        fout.write(bytearray([0 for i in range(0, 64)])) # 補間パラメータ(64Byte)
+        fout.write(bytearray([0 for i in range(0, 64)]))
 
 class VmdInfoIk():
     def __init__(self, name='', onoff=0):
@@ -40,10 +41,10 @@ class VmdShowIkFrame():
         fout.write(struct.pack('<L', self.frame))
         fout.write(struct.pack('b', self.show))
         fout.write(struct.pack('<L', len(self.ik)))
-        for k in (self.ik):
+        for k in self.ik:
             name = k.name.encode('ms932')
             fout.write(name)
-            fout.write(bytearray([0 for i in range(len(name), 20)])) # IKボーン名20Byteの残りを\0で埋める
+            fout.write(bytearray([0 for i in range(len(name), 20)]))
             fout.write(struct.pack('b', k.onoff))
         
 class VmdWriter():
@@ -51,20 +52,17 @@ class VmdWriter():
         pass
 
     def write_vmd_file(self, filename, bone_frames, showik_frames):
-        """Write VMD data to a file"""
         fout = open(filename, 'wb')
-        # header
         fout.write(b'Vocaloid Motion Data 0002\x00\x00\x00\x00\x00')
         fout.write(b'Dummy Model Name    ')
-        # bone frames
-        fout.write(struct.pack('<L', len(bone_frames))) # ボーンフレーム数
+        fout.write(struct.pack('<L', len(bone_frames)))
         for bf in bone_frames:
             bf.write(fout)
-        fout.write(struct.pack('<L', 0)) # 表情キーフレーム数
-        fout.write(struct.pack('<L', 0)) # カメラキーフレーム数
-        fout.write(struct.pack('<L', 0)) # 照明キーフレーム数
-        fout.write(struct.pack('<L', 0)) # セルフ影キーフレーム数
-        fout.write(struct.pack('<L', len(showik_frames))) # モデル表示・IK on/offキーフレーム数
+        fout.write(struct.pack('<L', 0))
+        fout.write(struct.pack('<L', 0))
+        fout.write(struct.pack('<L', 0))
+        fout.write(struct.pack('<L', 0))
+        fout.write(struct.pack('<L', len(showik_frames)))
         for sf in showik_frames:
             sf.write(fout)
         fout.close()
